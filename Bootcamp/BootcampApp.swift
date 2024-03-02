54e6f4017e9ea4bb749c81e41810feaeb54b02e2
MksMil <maksymmiliaiev@gmail.com>
2024-03-02 06:10:37 +0200

    Initial Commit

---
+++ Bootcamp/BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootcampApp.swift
+//  Bootcamp
+//
+//  Created by Миляев Максим on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
